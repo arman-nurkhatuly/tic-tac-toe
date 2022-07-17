a2e8f9b57b616e16d1feb8f87fb5b409365c55f6
Arman <arm.nnn04@gmail.com>
2022-07-17 22:56:04 +0600

    added reset button and mobile responsive

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const items = document.querySelectorAll('.item');
 const displayPlayer = document.querySelector('.display-player')
+const resetBtn = document.querySelector('#reset-btn');
 let playerStatus = document.querySelector('#player-status');
-let currentPlayer = 'X'
-let xCount = 0;
-let oCount = 0;
-let steps = 9;
-let gameOver = false;
+let currentPlayer = 'X';
 let board = ['', '', '', '', '', '', '', '', '',];
 let isGameActive = true;
+let playerWon = false;
 
 const winCases = [
     [0, 1, 2],
@@ -35,16 +33,14 @@ items.forEach((item, index) => {
     })
 })
 
-function whoWin(player){
-    switch(player){
+function whoWin(player) {
+    switch (player) {
         case 'X':
             alert('Игрок Х выиграл!')
             break;
         case 'O':
             alert('Игрок О выиграл!');
             break;
-        default:
-            alert('Ничья')
     }
 }
 
@@ -56,7 +52,6 @@ function changePlayer() {
 }
 
 function fnGameOver() {
-    let playerWon = false;
     for (let i = 0; i <= 7; i++) {
         let winCase = winCases[i];
         let a = board[winCase[0]]
@@ -72,16 +67,34 @@ function fnGameOver() {
     }
 
     if (playerWon) {
-        whoWin(currentPlayer == 'X' ? 'X' : 'O')
+        whoWin(currentPlayer == 'O' ? 'X' : 'O')
         isGameActive = false
     }
 
-    if(!board.includes('')){
+    if (!board.includes('') && isGameActive == true) {
         alert('Ничья')
         playerStatus.innerHTML = 'Ничья'
+        isGameActive = false
     }
 }
 
 function updateBoard(idx) {
     board[idx] = currentPlayer
+}
+
+resetBtn.onclick = function () {
+    if (isGameActive == false) {
+        board = ['', '', '', '', '', '', '', '', '',];
+        items.forEach(item => {
+            item.innerHTML = ''
+            item.classList.remove('true')
+            item.classList.add('false')
+        })
+        isGameActive = true
+        currentPlayer = 'X'
+        displayPlayer.innerHTML = 'X'
+        if(playerWon){
+            playerWon = false
+        }
+    }
 }
